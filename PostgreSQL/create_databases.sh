39edd25771bc8b0ebb1db385e36e66ba1793b37b
GunterMueller <gunter.mueller@gmail.com>
2017-04-10 22:22:44 -0400

    Postgress Modification for DW tables

---
+++ PostgreSQL/create_databases.sh
@@ -0,0 +1,3 @@
+#
+createdb dataware
+createdb source
--- PostgreSQL/create_dw_tables.sql
+++ PostgreSQL/create_dw_tables.sql
@@ -6,16 +6,18 @@
 
 /* default to dw database                                            */
 
-USE dw;
+/* USE dw; */
 
 /* creating customer_dim table                                       */
 
+DROP TABLE customer_dim 
+;
 CREATE TABLE customer_dim 
-( customer_sk INT NOT NULL AUTO_INCREMENT PRIMARY KEY
+( customer_sk INT NOT NULL  PRIMARY KEY
 , customer_number INT
 , customer_name CHAR(50)
 , customer_street_address CHAR(50)
-, customer_zip_code INT(5)
+, customer_zip_code INT
 , customer_city CHAR(30)
 , customer_state CHAR(2)
 , effective_date DATE
@@ -24,8 +26,10 @@ CREATE TABLE customer_dim
 
 /* creating product_dim table                                                */
 
+DROP TABLE product_dim 
+;
 CREATE TABLE product_dim 
-( product_sk INT NOT NULL AUTO_INCREMENT PRIMARY KEY
+( product_sk INT NOT NULL PRIMARY KEY
 , product_code INT
 , product_name CHAR(30)
 , product_category CHAR(30)
@@ -35,8 +39,10 @@ CREATE TABLE product_dim
 
 /* creating order_dim table                                          */
 
+DROP TABLE order_dim 
+;
 CREATE TABLE order_dim 
-( order_sk INT NOT NULL AUTO_INCREMENT PRIMARY KEY
+( order_sk INT NOT NULL PRIMARY KEY
 , order_number INT
 , effective_date DATE
 , expiry_date DATE )
@@ -44,19 +50,23 @@ CREATE TABLE order_dim
 
 /* creating date_dim table                                           */
 
+DROP TABLE date_dim 
+;
 CREATE TABLE date_dim 
-( date_sk INT NOT NULL AUTO_INCREMENT PRIMARY KEY
+( date_sk INT NOT NULL PRIMARY KEY
 , date DATE
 , month_name CHAR(9)
-, month INT(1)
-, quarter INT(1)
-, year INT(4)
+, month INT
+, quarter INT
+, year INT
 , effective_date DATE
 , expiry_date DATE )
 ;
 
 /* creating sales_order_fact_table                                   */
 
+DROP TABLE sales_order_fact 
+;
 CREATE TABLE sales_order_fact 
 ( order_sk INT 
 , customer_sk INT 
