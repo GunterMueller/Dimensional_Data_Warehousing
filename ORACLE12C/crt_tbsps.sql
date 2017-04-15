996a0987eebe4dbaec0f4a1954c7d025825e5e09
GunterMueller <gunter.mueller@gmail.com>
2017-04-15 19:42:15 -0400

    Updated Oracle SQL scripts to plugable db's

--- ORACLE12C/crt_tbsps.sql
+++ ORACLE12C/crt_tbsps.sql
@@ -1,4 +1,7 @@
 --
+ALTER SESSION SET CONTAINER = gmoradb1pdb1
+;
+----
 drop tablespace source_data_ts INCLUDING CONTENTS AND DATAFILES
 ;
 drop tablespace source_idx_ts INCLUDING CONTENTS AND DATAFILES
@@ -11,15 +14,15 @@ drop tablespace dw_idx_ts INCLUDING CONTENTS AND DATAFILES
 ;
 drop tablespace dw_temp_ts INCLUDING CONTENTS AND DATAFILES
 ;
-create tablespace source_data_ts DATAFILE '/u01/GUNTER/source_data_01.dbf' SIZE 512 M AUTOEXTEND ON
+create tablespace source_data_ts DATAFILE '/u01/GUNTER/PDB1/source_data_01.dbf' SIZE 512 M AUTOEXTEND ON
 ;
-create tablespace source_idx_ts DATAFILE '/u01/GUNTER/source_idx_01.dbf' SIZE 512 M AUTOEXTEND ON
+create tablespace source_idx_ts DATAFILE '/u01/GUNTER/PDB1/source_idx_01.dbf' SIZE 512 M AUTOEXTEND ON
 ;
-create temporary tablespace source_temp_ts TEMPFILE '/u01/GUNTER/source_temp_01.dbf' SIZE 512 M AUTOEXTEND ON
+create temporary tablespace source_temp_ts TEMPFILE '/u01/GUNTER/PDB1/source_temp_01.dbf' SIZE 512 M AUTOEXTEND ON
 ;
-create tablespace dataware_data_ts   DATAFILE '/u01/GUNTER/dataware_data_01.dbf' SIZE 512 M AUTOEXTEND ON
+create tablespace dataware_data_ts   DATAFILE '/u01/GUNTER/PDB1/dataware_data_01.dbf' SIZE 512 M AUTOEXTEND ON
 ;
-create tablespace dataware_idx_ts    DATAFILE  '/u01/GUNTER/dataware_idx_01.dbf' SIZE 512 M AUTOEXTEND ON
+create tablespace dataware_idx_ts    DATAFILE  '/u01/GUNTER/PDB1/dataware_idx_01.dbf' SIZE 512 M AUTOEXTEND ON
 ;
-create temporary tablespace dataware_temp_ts TEMPFILE '/u01/GUNTER/dataware_temp_01.dbf' SIZE 512 M AUTOEXTEND ON
+create temporary tablespace dataware_temp_ts TEMPFILE '/u01/GUNTER/PDB1/dataware_temp_01.dbf' SIZE 512 M AUTOEXTEND ON
 ;
--- ORACLE12C/crt_users.sql
+++ ORACLE12C/crt_users.sql
@@ -1,25 +1,28 @@
 --
-DROP USER C##DATAWARE
+ALTER SESSION SET CONTAINER = gmoradb1pdb1
 ;
-DROP USER C##SOURCE
+------------------------
+DROP USER DATAWARE
 ;
-CREATE USER C##DATAWARE IDENTIFIED BY DATAWARE
+DROP USER SOURCE
+;
+CREATE USER DATAWARE IDENTIFIED BY DATAWARE
 default tablespace dataware_data_ts
 temporary tablespace  dataware_temp_ts
 ;
-CREATE USER C##SOURCE IDENTIFIED BY SOURCE
+CREATE USER SOURCE IDENTIFIED BY SOURCE
 default tablespace  source_data_ts
 temporary tablespace source_temp_ts;
 ;
-grant create session to c##dataware  WITH ADMIN OPTION;
+grant create session to dataware  WITH ADMIN OPTION;
 ;
-grant create table to c##dataware
+grant create table to dataware
 ;
-grant unlimited tablespace to  c##dataware
+grant unlimited tablespace to  dataware
 ;
-grant create session to c##source  WITH ADMIN OPTION;
+grant create session to source  WITH ADMIN OPTION;
 ;
-grant create table to c##source
+grant create table to source
 ;
-grant unlimited tablespace to  c##source
+grant unlimited tablespace to  source
 ;
