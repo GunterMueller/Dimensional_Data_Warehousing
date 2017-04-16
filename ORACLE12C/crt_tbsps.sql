0417f1cec2c2f30ad0363095cff974ea1d6e2298
GunterMueller <gunter.mueller@gmail.com>
2017-04-16 10:55:54 -0400

    Corrected mispelling and create drop_tbspaces script.

--- ORACLE12C/crt_tbsps.sql
+++ ORACLE12C/crt_tbsps.sql
@@ -8,11 +8,11 @@ drop tablespace source_idx_ts INCLUDING CONTENTS AND DATAFILES
 ;
 drop tablespace source_temp_ts INCLUDING CONTENTS AND DATAFILES
 ;
-drop tablespace dw_data_ts INCLUDING CONTENTS AND DATAFILES
+drop tablespace dataware_data_ts INCLUDING CONTENTS AND DATAFILES
 ;
-drop tablespace dw_idx_ts INCLUDING CONTENTS AND DATAFILES
+drop tablespace dataware_idx_ts INCLUDING CONTENTS AND DATAFILES
 ;
-drop tablespace dw_temp_ts INCLUDING CONTENTS AND DATAFILES
+drop tablespace dataware_temp_ts INCLUDING CONTENTS AND DATAFILES
 ;
 create tablespace source_data_ts DATAFILE '/u01/GUNTER/PDB1/source_data_01.dbf' SIZE 512 M AUTOEXTEND ON
 ;
---
+++ ORACLE12C/drop_tbsps.sql
@@ -0,0 +1,14 @@
+--
+----
+drop tablespace source_data_ts INCLUDING CONTENTS AND DATAFILES
+;
+drop tablespace source_idx_ts INCLUDING CONTENTS AND DATAFILES
+;
+drop tablespace source_temp_ts INCLUDING CONTENTS AND DATAFILES
+;
+drop tablespace dataware_data_ts INCLUDING CONTENTS AND DATAFILES
+;
+drop tablespace dataware_idx_ts INCLUDING CONTENTS AND DATAFILES
+;
+drop tablespace dataware_temp_ts INCLUDING CONTENTS AND DATAFILES
+;
